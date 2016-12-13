b331747d02a810a62ce5366c7faf87259f03452a
Auguste Dutcher <Aug@Augustes-MacBook-Pro.local>
2016-12-13 14:49:05 -0800

    first complete draft

--- find_promoters.py
+++ find_promoters.py
@@ -14,16 +14,56 @@ with open('sample_tss_list.csv', 'r') as TSS_input:
     next(TSS_reader, None) # ignore the header row
     for row in TSS_reader:
         TSS_list.append((row[0], row[1], int(row[2])))
-
+print(TSS_list)
 
 # Promoter sequences to look for: A2 and T6 always conserved plus 2 out of 4 variable sites (total of 4/6).
-promoters = ['TATNNT', 'TANANT', 'TANNAT', 'NATANT', 'NATNAT', 'NANAAT']
+promoters_fwd = ['TATNNT', 'TANANT', 'TANNAT', 'NATANT', 'NATNAT', 'NANAAT']
+promoters_rev = ['ANNATA', 'ANTNTA', 'ATNNTA', 'ANTATN', 'ATNATN', 'ATTNTN']
 
 # Length to extend the search window (upstream and downstream of sRNA TSS)
-upstream_window = 50
-downstream_windown = 10
+upstream_margin = 2
+downstream_margin = 10
 
-sample_seq = Seq('ATCGTGTCGATAGCTAGACTTAGGCTAGCTGATCGACTAGCTAGCTAGCA', IUPAC.unambiguous_dna)
-print(sample_seq[5:20])
-print(sample_seq[5:20].reverse_complement())
+sample_seq = Seq('ATCGTGTCGATAGCTAGACTTAGGCTAGCTGATCGACTAGCACTATCTAGCTATATATTGCA', IUPAC.unambiguous_dna)
+
+def search_for_substring(promoter_list, seq_to_search):
+    search_array = []
+    for promoter in promoter_list:
+        search_results = nt_search(str(seq_to_search), promoter)
+        search_array.append(search_results)
+    return search_array
+
+for entry in TSS_list:
+    sRNA_name = entry[0]
+    strand = entry[1]
+    TSS = entry[2]
+    if strand == 'F':
+        window_start_pos = TSS - 1 - upstream_margin
+        window_end_pos = TSS + downstream_margin
+        print('window start: ', window_start_pos)
+        window_to_search = sample_seq[window_start_pos:window_end_pos]
+        results = search_for_substring(promoters_fwd, window_to_search)
+        print(results)
+        for pos_list in results:
+            num_results = len(pos_list) - 1
+            if num_results >= 1:
+                for entry in pos_list[1:]:
+                    promoter_pos = window_start_pos + pos_list[1]
+                    print(promoter_pos)
+                    print(sample_seq[promoter_pos:promoter_pos + 6])
+    elif strand == 'R':
+        window_start_pos = TSS - 1 - downstream_margin
+        window_end_pos = TSS + upstream_margin
+        window_to_search = sample_seq[window_start_pos:window_end_pos]
+        results = search_for_substring(promoters_rev, window_to_search)
+        print(results)
+        for pos_list in results:
+            num_results = len(pos_list) - 1
+            if num_results >= 1:
+                for entry in pos_list[1:]:
+                    promoter_pos = window_start_pos + pos_list[1]
+                    print(promoter_pos)
+                    print(sample_seq[promoter_pos:promoter_pos + 6])
+                      
+#results_array.append((sRNA_name, strand, TSS, ))
 
